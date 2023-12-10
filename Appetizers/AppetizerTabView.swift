78e3e4013fd5f74aa07451cb545c1979b4a7799e
Diana Duong <dduong013@example.com>
2023-12-10 13:55:35 -0600

    create view files

---
+++ Appetizers/AppetizerTabView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Diana Duong on 12/10/23.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        
+        TabView { 
+            
+        }
+    }
+}
+
+#Preview {
+    AppetizerTabView()
+}
