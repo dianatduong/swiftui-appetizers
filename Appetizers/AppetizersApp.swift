2d81f22d9927d74ea427f2e1889aa7f60be91e99
Diana Duong <dduong013@example.com>
2023-12-10 13:46:10 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Diana Duong on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
