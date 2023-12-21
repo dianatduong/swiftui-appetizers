31ea4a6bb0f0bed8646a1612ee75dac83975593b
Diana Duong <dduong013@example.com>
2023-12-21 14:22:54 -0600

    networking call

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State private var appetizers: [AppetizerData] = []
+    
     var body: some View {
         
         NavigationView {
-            
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+        }
+        .onAppear {
+            getAppetizers()
+        }
+    }
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
