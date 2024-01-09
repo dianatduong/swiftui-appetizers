5a4d240199224a22a87b0cae4e5857673e96cc53
Diana Duong <dduong013@gmail.com>
2024-01-09 15:50:31 -0600

    add form toggles

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -13,6 +13,10 @@ struct AccountView: View {
     @State private var lastName = ""
     @State private var email = ""
     @State private var birthdate = Date()
+    
+    @State private var extraNapkin = false
+    @State private var frequentRefills = false
+
 
 
     
@@ -30,11 +34,18 @@ struct AccountView: View {
                             .textInputAutocapitalization(.none)
                             .disableAutocorrection(true)
                         DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                        
+                        Button {
+                            print("save")
+                        } label: {
+                            Text("Save Changes")
+                        }
                     }
                     
                     Section(header: Text("Requests")) {
-                        Text("Extra Napkins")
-                        Text("Frequent Refils")
+                        Toggle("Extra Napkins", isOn: $extraNapkin)
+                        Toggle("Frequent Refils", isOn: $frequentRefills)
+
                     }
                 }
             }
